a33590c230d6765f3c45db24531db56383c15caf
0xmarcin <0xmarcin+gh@gmail.com>
2020-06-28 20:45:00 +0200

    Empty project

---
+++ src/test/java/pl/marcinchwedczuk/expr/AppTest.java
@@ -0,0 +1,10 @@
+package pl.marcinchwedczuk.expr;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testAppHasAGreeting() {
+        assertEquals(true, true);
+    }
+}
